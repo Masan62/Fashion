7bef9420b8d57042fffaa4fe7ecb3cefd5522ac1
Masan62 <razavim1@msu.edu>
2025-02-22 17:48:32 +0000

    Added the images and the interaction

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Add JavaScript below
+window.onload = function() {
+   
+    ScrollReveal().reveal('.reveal', {
+      interval: 500,  
+      delay: 500,    
+      duration: 1000  
+    });
+  };
+ 
